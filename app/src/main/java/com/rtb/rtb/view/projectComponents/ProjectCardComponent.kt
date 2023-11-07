f531c0c3c6952e4d40868938b3a57f7109481bb4
Robson019 <reep55555@gmail.com>
2023-11-07 14:40:02 -0300

    Add SignUpMessages module and fix project views (#15)

--- app/src/main/java/com/rtb/rtb/view/projectComponents/ProjectCardComponent.kt
+++ app/src/main/java/com/rtb/rtb/view/projectComponents/ProjectCardComponent.kt
@@ -9,8 +9,18 @@ class ProjectCardComponent : AppCompatActivity() {
     private val binding by lazy {
         ComponentProjectCardBinding.inflate(layoutInflater)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val selectProject = binding.pcConstraintLayoutProjectContent
+        selectProject.setOnClickListener {
+            //TODO: Implement me
+        }
     }
 }
